3ba55cf851c0425f797dbfc7d98e11fc8af3df2e
NguyenTriBaoThang <nguyentribaothang@gmail.com>
2026-03-12 03:27:53 +0700

    feat: create train controller

---
+++ src/Childsafenet.Api/Childsafenet.Api/Controllers/TrainController.cs
@@ -0,0 +1,32 @@
+﻿using Childsafenet.Api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Childsafenet.Api.Controllers;
+
+[Authorize(Roles = "admin")]
+[ApiController]
+[Route("api/train")]
+public class TrainController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public TrainController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("jobs")]
+    public IActionResult GetJobs()
+    {
+        // For now, return an empty array until backend ML scheduling is fully built
+        return Ok(new object[] { });
+    }
+
+    [HttpPost("trigger")]
+    public IActionResult TriggerTrain()
+    {
+        // Return a mocked success response
+        return Ok(new { jobId = "mock_train_job_" + DateTime.UtcNow.Ticks, status = "Queued" });
+    }
+}
